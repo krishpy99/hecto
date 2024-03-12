dcd523eb01e77ef5e41e8e9148250f4a0e96b1d0
Lai-YT <381xvmvbib@gmail.com>
2024-03-12 14:02:42 +0800

    Separate code into multiple files

---
+++ src/editor.rs
@@ -0,0 +1,40 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        // As long as this variable is alive, we are in raw mode.
+        // For information on what are terminal modes, see
+        // https://www.gnu.org/software/mit-scheme/documentation/stable/mit-scheme-ref/Terminal-Mode.html.
+        let _raw_stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(e) => die(e),
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {}
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
+
+use editor::Editor;
 
 fn main() {
-    // As long as this variable is alive, we are in raw mode.
-    // For information on what are terminal modes, see
-    // https://www.gnu.org/software/mit-scheme/documentation/stable/mit-scheme-ref/Terminal-Mode.html.
-    let _raw_stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(e) => die(e),
-        }
-    }
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
+    let editor = Editor::default();
+    editor.run();
 }
