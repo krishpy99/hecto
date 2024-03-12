2d9aff63bf1f78d750c53314f922f6fbb9ffbbf3
Lai-YT <381xvmvbib@gmail.com>
2024-03-12 14:43:41 +0800

    Show welcome message when entering the editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,36 +1,83 @@
-use std::io::{self, stdout};
+use crate::terminal::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        // As long as this variable is alive, we are in raw mode.
-        // For information on what are terminal modes, see
-        // https://www.gnu.org/software/mit-scheme/documentation/stable/mit-scheme-ref/Terminal-Mode.html.
-        let _raw_stdout = stdout().into_raw_mode().unwrap();
-
         loop {
-            if let Err(e) = self.process_keypress() {
+            // NOTE: The screen is refreshed before quitting.
+            if let Err(e) = &self.refresh_screen() {
                 die(e);
             }
             if self.should_quit {
                 break;
             }
+            if let Err(e) = &self.process_keypress() {
+                die(e);
+            }
         }
     }
 
     pub fn default() -> Self {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide(); // prevent the cursor from blinking
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            Terminal::clear_screen();
+            Editor::farewell();
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn farewell() {
+        println!("Goodbye.\r");
+    }
+
+    /// Draws a tilde in each row, meaning that row is not part of the file and can't contain any text
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        // The last line is kept empty for the status bar.
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_msg = format!("Hecto editor -- version {VERSION}");
+        let term_width = self.terminal.size().width as usize;
+        let msg_len = welcome_msg.len();
+        // The padding is the number of spaces to add to the left of the message.
+        let padding = term_width.saturating_sub(msg_len) / 2;
+        let spaces = " ".repeat(padding.saturating_add(1 /* for ~ */));
+        welcome_msg = format!("~{spaces}{welcome_msg}\r");
+        welcome_msg.truncate(term_width);
+        println!("{welcome_msg}\r");
     }
 
     /// Where the handling logics go.
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             // Getting a `quit` signal isn't an error.
             Key::Ctrl('q') => {
@@ -42,14 +89,7 @@ impl Editor {
     }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
-fn die(e: std::io::Error) {
+fn die(e: &std::io::Error) {
+    Terminal::clear_screen();
     panic!("{}", e);
 }
---
+++ src/terminal.rs
@@ -0,0 +1,69 @@
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+    _raw_stdout: RawTerminal<std::io::Stdout>,
+}
+
+impl Terminal {
+    pub fn default() -> Result<Self, std::io::Error> {
+        let size = termion::terminal_size()?;
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            // As long as this variable is alive, we are in raw mode.
+            // For information on what are terminal modes, see
+            // https://www.gnu.org/software/mit-scheme/documentation/stable/mit-scheme-ref/Terminal-Mode.html.
+            _raw_stdout: stdout().into_raw_mode()?,
+        })
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+
+    /// The position is 0-based.
+    pub fn cursor_position(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    pub fn size(&self) -> &Size {
+        &self.size
+    }
+}
